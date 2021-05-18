50b4f8e5a7909af05ecd6c8f9f6f52726ad60252
Yi Liu <yiliu6@microsoft.com>
2021-05-18 16:37:33 +0800

    run with kv starter and spring cloud 2020.0.2

--- gateway/src/main/java/com/secure/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/secure/gateway/GatewayApplication.java
@@ -1,13 +1,24 @@
 package com.secure.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+    @Value("${test-secret}")
+    private String testSecret;
+
+    public void run(String[] args) {
+        LOGGER.info("property springDataSourceUrl in Azure Key Vault: " + testSecret);
+    }
 }
